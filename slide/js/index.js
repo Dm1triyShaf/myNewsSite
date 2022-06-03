a763164ee68ae230b8fb3a7bc0ba14c4849c8bb9
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-06-04 01:28:08 +0300

    firstCommit

--- slide/js/index.js
+++ slide/js/index.js
@@ -1,1 +1,3 @@
-slider1.start();
+var slider1 =  sliderFactory.createNewSlider();
+
+slider1.start('its-slider1');
--- slide/js/slide.js
+++ slide/js/slide.js
@@ -1,50 +1,66 @@
-
-var slider1 = {
-    imagesUrls: [],
-    currentImageIndex: 0,
-    showPrevBtn: document.getElementById('show-prev-btn'),
-    showNextBtn: document.getElementById('show-next-btn'),
-    slideImage: document.getElementById('slide-img'),
-
-    start: function () {
-        var that = this;
-        this.showPrevBtn.addEventListener('click', function (e) {
-            that.onShowPrevBtnClick(e)
-        });
-
-        this.showNextBtn.addEventListener('click', function (e) {
-            that.onShowNextBtnClick(e)
-        });
+var sliderFactory = {
+   
+    createNewSlider: function () {  
+        var newSlider = {
+            imagesUrls: [],
+            currentImageIndex: 0,
+            
+            showPrevBtn: null,
+            showNextBtn: null,
+            slideImage: null,
+        
+            start: function (elId) {
+                var that = this;
+        
+                var elSelector = '#' + elId;
+                var el = document.querySelector(elSelector);
+        
+                this.showPrevBtn = el.querySelector('.show-prev-btn');
+                this.showNextBtn = el.querySelector('.show-next-btn');
+                this.slideImage = el.querySelector('.slide-img');
+        
+                this.showPrevBtn.addEventListener('click', function (e) {
+                    that.onShowPrevBtnClick(e)
+                });
+        
+                this.showNextBtn.addEventListener('click', function (e) {
+                    that.onShowNextBtnClick(e)
+                });
+        
+                this.imagesUrls.push('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1b5hi5Fpdcy6XuyeuZ9I6k_WQUk5AjUAuZg&usqp=CAU');
+                this.imagesUrls.push('https://1gai.ru/uploads/posts/2020-09/1601381443_56.jpg');
+                this.imagesUrls.push('https://zelenyjmir.ru/wp-content/uploads/2017/06/Mops-17.jpg');
+                this.imagesUrls.push('https://99px.ru/sstorage/53/2012/08/mid_47709_5099.jpg');
+        
+                this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+                this.showPrevBtn.disablet = true;
+            },
+        
+            onShowPrevBtnClick: function (e) {
+                this.currentImageIndex--;
+                this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+                this.showNextBtn.disablet = false;
+        
+                if (this.currentImageIndex === 0) {
+                    this.showPrevBtn.disablet = true;
+                }
+            },
+        
+            onShowNextBtnClick: function (e) {
+                this.currentImageIndex++;
+                this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+                this.showPrevBtn.disablet = false;
+        
+                if (this.currentImageIndex == (this.imagesUrls.length - 1)) {
+                    this.showPrevBtn.disablet = true;
+                }
+            }
+       
+        };
     
-        this.imagesUrls.push('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1b5hi5Fpdcy6XuyeuZ9I6k_WQUk5AjUAuZg&usqp=CAU');
-        this.imagesUrls.push('https://1gai.ru/uploads/posts/2020-09/1601381443_56.jpg');
-        this.imagesUrls.push('https://zelenyjmir.ru/wp-content/uploads/2017/06/Mops-17.jpg');
-        this.imagesUrls.push('https://99px.ru/sstorage/53/2012/08/mid_47709_5099.jpg');
-
-        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
-        this.showPrevBtn.disablet = true;
-    },
-
-    onShowPrevBtnClick: function(e) {
-        this.currentImageIndex--;
-        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
-        this.showNextBtn.disablet = false;
-
-        if (this.currentImageIndex === 0) {
-            this.showPrevBtn.disablet = true;
-        }
-    },
-
-    onShowNextBtnClick: function(e) {
-        this.currentImageIndex++;
-        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
-        this.showPrevBtn.disablet = false;
-
-        if (this.currentImageIndex === (this.imagesUrls.length - 1) ) {
-            this.showNextBtn.disablet = true;
-        }
+        return newSlider;
     }
-};
+    }
 
 
 
